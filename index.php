b81f89876c4c948727d071b9bf9e4895a64f4104
tien tran <tranmanhtien410@gmail.com>
2019-05-21 19:24:06 +0700

    bai1

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tien tran
- * Date: 5/21/2019
- * Time: 6:35 PM
- */
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <form name="bmi">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Chieu cao(cm)</label>
+                    <input type="text" class="form-control"  name="hight" value="">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Can nang(kg)</label>
+                    <input type="password" class="form-control" name="weight">
+                </div>
+                <button type="submit" class="btn btn-primary">Tính</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
