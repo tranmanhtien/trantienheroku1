705ac97d26399872b8f446780c6a83719296f8d0
tien tran <tranmanhtien410@gmail.com>
2019-05-21 19:37:23 +0700

    bai1

--- index.php
+++ index.php
@@ -6,20 +6,30 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<?php
+if(isset($_POST["height"]&&isset($_POST["weight"]){
+    $weight=(int) $_POST["weight"];
+    $height=(int) $_POST["height"];
+    $bmi=$weight/($height *$height);
+    echo "chỉ số pmi".$bmi;
+}
+?>
+
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            <form name="bmi">
+            <h3>Tính chỉ số bmi</h3>
+            <form name="bmi" method="post">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Chieu cao(cm)</label>
-                    <input type="text" class="form-control"  name="hight" value="">
+                    <input type="text" class="form-control"  name="height" value="">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Can nang(kg)</label>
                     <input type="password" class="form-control" name="weight">
                 </div>
-                <button type="submit" class="btn btn-primary">Tính</button>
+                <button type="submit" name="calo" class="btn btn-primary">Tính</button>
             </form>
         </div>
     </div>
