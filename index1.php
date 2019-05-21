e92f3381aa32769d965ce99ec6c5eb02ee74ce82
tien tran <tranmanhtien410@gmail.com>
2019-05-21 20:02:59 +0700

    bai1

--- index1.php
+++ index1.php
@@ -1,8 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
 <?php
-/**
- * Created by PhpStorm.
- * User: tien tran
- * Date: 5/21/2019
- * Time: 6:54 PM
- */
-echo "Deploy tu dong";
+$weight=$height='';
+if(isset($_GET["height"]) && isset($_GET["weight"])){
+    $weight = $_GET["weight"];
+    $height = $_GET["height"];
+    $bmi = $weight/($height * $height);
+    echo "chỉ số bmi của bạn:".$bmi."<br>";
+    if($bmi<18.5){
+        echo"bạn thuộc loại gầy";
+    } elseif ($bmi>=18.5 && $bmi<=24.9){
+        echo"bạn thuộc loại bình thường";
+    }elseif ($bmi>=25 && $bmi<=29.9){
+        echo"bạn thuộc loại hơi béo";
+    }elseif ($bmi>=30 && $bmi<=34.9){
+        echo"bạn thuộc loại béo phì cấp độ 1";
+    }elseif ($bmi>=35 && $bmi<=39.9){
+        echo"bạn thuộc loại béo phì cấp độ 2";
+    }elseif ($bmi>=40){
+        echo"bạn thuộc loại béo phì cấp độ 3";
+    }
+}
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Tính chỉ số bmi</h3>
+            <form name="bmi" method="get">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Chiều cao(m)</label>
+                    <input type="text" class="form-control"  name="height" value="<?php echo $height ?>">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Cân nặng(kg)</label>
+                    <input type="text" class="form-control" name="weight" value="<?php echo $weight ?>">
+                </div>
+                <button type="submit" name="calo" class="btn btn-primary">Tính</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
